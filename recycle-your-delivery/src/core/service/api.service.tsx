6ba638cb94467532ea054366786c7a099ad5bf3f
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2021-05-20 11:17:23 +0300

    refactoring axios request

--- recycle-your-delivery/src/core/service/api.service.tsx
+++ recycle-your-delivery/src/core/service/api.service.tsx
@@ -1,113 +1,54 @@
-import axios from 'axios';
-import { SolicitacaoModel } from '../model/solicitacao.model';
+import axios from "axios";
+import { SolicitacaoModel } from "../model/solicitacao.model";
 
-function salvarSolicitacao(solicitacao: SolicitacaoModel): Promise<SolicitacaoModel> {
+const api = axios.create({
+  baseURL: "http://10.0.2.2:8080/v1",
+  responseType: "json",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-    const api = axios.create({
-        baseURL: 'http://10.0.2.2:8080/v1',
-        responseType: 'json',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-
-    return api.post(`/solicitacao`, JSON.stringify(solicitacao));
-
+function salvarSolicitacao(
+  solicitacao: SolicitacaoModel
+): Promise<SolicitacaoModel> {
+  return api.post(`/solicitacao`, JSON.stringify(solicitacao));
 }
-
 
 function listarSolicitacoes(idUser: string): Promise<Array<SolicitacaoModel>> {
-
-    const api = axios.create({
-        baseURL: 'http://10.0.2.2:8080/v1',
-        responseType: 'json',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-
-    return api.get(`/solicitacao/listar-solicitacoes/${idUser}`);
-
+  return api.get(`/solicitacao/listar-solicitacoes/${idUser}`);
 }
 
-function encontrarSolicitacoes(idUser: string): Promise<Array<SolicitacaoModel>> {
-
-    const api = axios.create({
-        baseURL: 'http://10.0.2.2:8080/v1',
-        responseType: 'json',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-
-    return api.get(`/solicitacao/encontrar-solicitacoes/${idUser}`);
-
+function encontrarSolicitacoes(
+  idUser: string
+): Promise<Array<SolicitacaoModel>> {
+  return api.get(`/solicitacao/encontrar-solicitacoes/${idUser}`);
 }
 
 function getSolicitacao(id: number): Promise<Array<SolicitacaoModel>> {
-
-    const api = axios.create({
-        baseURL: 'http://10.0.2.2:8080/v1',
-        responseType: 'json',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-
-    return api.get(`/solicitacao/${id}`);
-
+  return api.get(`/solicitacao/${id}`);
 }
 
 function ativarSolicitacao(id: number): Promise<SolicitacaoModel> {
-
-    const api = axios.create({
-        baseURL: 'http://10.0.2.2:8080/v1',
-        responseType: 'json',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-
-    return api.put(`/solicitacao/${id}/ativar`, {});
-
+  return api.put(`/solicitacao/${id}/ativar`, {});
 }
 
 function emAndamentoSolicitacao(id: number): Promise<SolicitacaoModel> {
-
-    const api = axios.create({
-        baseURL: 'http://10.0.2.2:8080/v1',
-        responseType: 'json',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-
-    return api.put(`/solicitacao/${id}/em-andamento`, {});
-
+  return api.put(`/solicitacao/${id}/em-andamento`, {});
 }
 
 function cancelarSolicitacao(id: number): Promise<SolicitacaoModel> {
-
-    const api = axios.create({
-        baseURL: 'http://10.0.2.2:8080/v1',
-        responseType: 'json',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-
-    return api.put(`/solicitacao/${id}/cancelar`, {});
-
+  return api.put(`/solicitacao/${id}/cancelar`, {});
 }
 
 const apiService = {
-    salvarSolicitacao,
-    listarSolicitacoes,
-    getSolicitacao,
-    ativarSolicitacao,
-    emAndamentoSolicitacao,
-    cancelarSolicitacao,
-    encontrarSolicitacoes
-}
+  salvarSolicitacao,
+  listarSolicitacoes,
+  getSolicitacao,
+  ativarSolicitacao,
+  emAndamentoSolicitacao,
+  cancelarSolicitacao,
+  encontrarSolicitacoes,
+};
 
 export default apiService;
